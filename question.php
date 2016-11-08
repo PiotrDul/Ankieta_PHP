33f619757d41ff3d92010bd066a5afb1c4958f6f
Michal Strojek <piro@student.edu>
2016-11-08 15:59:24 +0100

    pierwsze klasy

---
+++ question.php
@@ -0,0 +1,28 @@
+<?php
+
+class question{
+    private $question;
+    
+    
+    public function returnAllAnswers(){
+        
+    }
+    
+    public function 
+    
+    
+}
+/* 
+ Pytanie:
+
+Ma mieć tekst pytania.
+Ma implementować metody:
+zwracającą wszystkie udzielone odpowiedzi na to pytanie,
+zmieniające tekst pytania, zwracające tekst pytania,
+zapamiętujące pytanie do bazy danych.
+Ma implementować statyczne metody:
+stworzenie nowego pytania (potrzebuje podania id ankiety),
+wczytanie pytania o podanym id z bazy danych,
+usunięcie pytania o podanym id z bazy danych.
+ */
+
